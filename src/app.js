8170b0b7858e8caa829e8fd5bbf149bcb8c364fb
Sebastian Sarria <161655160+JSebasSN@users.noreply.github.com>
2024-05-26 19:29:25 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,49 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function numAleatorio() {
+    let letNumAleatorio = [
+      "A",
+      "Q",
+      "K",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "J"
+    ];
+
+    let num = Math.floor(Math.random() * letNumAleatorio.length);
+
+    return letNumAleatorio[num];
+  }
+
+  let simbolos = [
+    "src/img/heart.png",
+    "src/img/diamonds.png",
+    "src/img/clover.png",
+    "src/img/heart_red.png",
+    "src/img/clover_red.png",
+    "src/img/diamonds_red.png"
+  ];
+
+  function objAletorio() {
+    let simbol = Math.floor(Math.random() * simbolos.length);
+    return simbolos[simbol];
+  }
+
+  document.getElementById("change").addEventListener("click", function() {
+    let numero = numAleatorio();
+    let imagen = objAletorio();
+
+    document.getElementById("simbIni").src = imagen;
+    document.getElementById("simbFin").src = imagen;
+    document.getElementById("numCard").innerHTML = numero;
+  });
 };
